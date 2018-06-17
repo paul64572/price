8b5e8f86e70ae2641f0b2fa718ea76d8f4575e7e
Priyesh Shah <ps2@dircon.co.uk>
2018-06-17 14:58:21 +0100

    init

--- src/main/java/com/trading/Price.java
+++ src/main/java/com/trading/Price.java
@@ -1,10 +1,25 @@
 package com.trading;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Price {
 
     public static void main( String... args )
     {
         System.out.println("Hello world");
-
+        SpringApplication.run(Price.class, args);
     }
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello there";
+    }
+
 }
